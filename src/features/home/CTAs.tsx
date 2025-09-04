7d34de0a922a9bc1db4694aeedba7e2f85c6bc22
sssiima <viktoria.simanchuk@gmail.com>
2025-09-04 10:37:38 +0300

    from home to auth

--- src/features/home/CTAs.tsx
+++ src/features/home/CTAs.tsx
@@ -8,7 +8,7 @@ const CTAs = () => {
       <button onClick={() => navigate('/reg')} className="w-[350px] h-[66px] rounded-3xl bg-brand hover:bg-teal-600 text-white font-bold text-[22px]">
         Хочу участвовать!
       </button>
-      <button 
+      <button onClick={() => navigate('/auth')}
         className="w-[350px] h-[66px] rounded-3xl bg-transparent border-2 border-brand text-brand font-medium text-[22px]"
       >
         Авторизация
