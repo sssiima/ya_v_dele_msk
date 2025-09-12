a8f14dbbd45a283ba6b585766eabc44449d731c8
sssiima <viktoria.simanchuk@gmail.com>
2025-09-13 02:21:19 +0300

    added profile page frontend (in process)

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -4,8 +4,9 @@ const ProfilePage = () => {
   // const { user } = useTelegram()
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-brand flex items-center justify-center text-center">
       {/* Страница полностью пустая с белым фоном */}
+      <h1 className="text-white text-2xl font-bold">Вся информация о профиле скоро появится</h1>
     </div>
   )
 }
