97b63d4a69df6771d387696e4a8d435895a86659
sssiima <viktoria.simanchuk@gmail.com>
2025-10-24 02:40:17 +0300

    teampage

--- src/components/TeamPage.tsx
+++ src/components/TeamPage.tsx
@@ -1,14 +1,12 @@
 const Ro = ({ title, image }: { title?: string, image?: string }) => {
     
     return (
-        <div className="flex justify-center">
-        <div className="w-full max-w-[95px]">
-          <img src={image} alt='user' className="w-full rounded-xl bg-gray-200 w-[70px] h-28" />
-          <div className="text-[8px] text-center text-black mt-1">
+        <div className="w-[70px] flex flex-col items-center justify-center sm:w-32">
+          <img src={image} alt='user' className="rounded-xl bg-gray-200 w-[70px] h-28 sm:w-32 sm:h-48" />
+          <div className="text-[6px] text-center text-black mt-1 sm:text-[10px]">
                 {title}
           </div>
         </div>
-      </div>
     )
   }
 
@@ -16,12 +14,12 @@ const Ro = ({ title, image }: { title?: string, image?: string }) => {
     
     return (
         <div className="flex justify-center">
-        <div className="w-full max-w-[95px]">
-          <img src={image} alt='user' className="w-full rounded-xl bg-gray-200 w-[70px] h-28" />
-          <div className="text-[8px] text-center text-black mt-1">
+        <div className="w-[70px] flex flex-col items-center justify-center sm:w-32">
+          <img src={image} alt='user' className="rounded-xl bg-gray-200 w-[70px] h-28 sm:w-32 sm:h-48" />
+          <div className="text-[6px] text-center text-black mt-1 sm:text-[10px]">
                 {title}
           </div>
-          <div className="text-[6px] text-center text-black">
+          <div className="text-[4px] text-center text-black sm:text-[8px]">
                 {subtitle}
           </div>
         </div>
@@ -68,14 +66,44 @@ const TeamPage = () => {
             <div>
                 <h3 className="normal-case mt-4 text-[12px]">Координаторы</h3>
                 <div className="flex flex-wrap justify-center gap-3 mt-4">
-                    <Ro title='Навакова Мария' />
-                    <Ro title='Навакова Мария' />
-                    <Ro title='Навакова Мария' />
-                    <Ro title='Навакова Мария' />
-                    <Ro title='Навакова Мария' />
-                    <Ro title='Навакова Мария' />
-                    <Ro title='Навакова Мария' />
-                    <Ro title='Навакова Мария' />
+                    <Ro title='Бенескул Архип' image='images/Бенескул_Архип.jpg'/>
+                    <Ro title='Кожурина Анастасия' image='images/Кожурина_Анастасия.jpg'/>
+                    <Ro title='Семернина Полина' image='images/Семернина_Полина.jpg'/>
+                    <Ro title='Слободина Юлия' image='images/Слободина_Юлия.jpg'/>
+                    <Ro title='Терентьев Артем' image='images/Терентьев_Артем.jpg'/>
+                    <Ro title='Алибашич Ясмина' image='images/Алибашич_Ясмина.jpg'/>
+                    <Ro title='Васенина Дария' image='images/Васенина_Дария.jpg'/>
+                    <Ro title='Пасько Александра' image='images/Пасько_Александра.jpg'/>
+                    <Ro title='Старовойтова Галина' image='images/Старовоитова_Галина.jpg'/>
+                    <Ro title='Шастина Злата' image='images/Шастина_Злата.jpg'/>
+                    <Ro title='Назарук Александра' image='images/Назарук_Александра.jpg'/>
+                    <Ro title='Осипова Наталья' image='images/Осипова_Наталья.jpg'/>
+                    <Ro title='Стрюк Станислав' image='images/Стрюк_Станислав.jpg'/>
+                    <Ro title='Губарева Софья' image='images/Губарева_Софья.jpg'/>
+                    <Ro title='Корнева Ксения' image='images/Корнева_Ксения.jpg'/>
+                    <Ro title='Милюхина Саша' image='images/Милюхина_Александра.jpg'/>
+                    <Ro title='Позмогова Ксения' image='images/Позмогова_Ксения.jpg'/>
+                    <Ro title='Шкурко Владислав' image='images/Шкурко_Владислав.jpg'/>
+                    <Ro title='Алиев Руслан' image='images/Алиев_Руслан.jpg'/>
+                    <Ro title='Бойко Илья' image='images/Бойко_Илья.jpg'/>
+                    <Ro title='Брызжина Кристина' image='images/Брызжина_Кристина.jpg'/>
+                    <Ro title='Салова Анна' image='images/Салова_Анна.jpg'/>
+                    <Ro title='Кокодеев Леонид' image='images/Кокодеев_Леонид.jpg'/>
+                    <Ro title='Малявко Илья' image='images/Малявко_Илья.jpg'/>
+                    <Ro title='Мошенко Артем' image='images/Мошенко_Артем.jpg'/>
+                    <Ro title='Ачето Максим' image='images/Ачето_Максим.jpg'/>
+                    <Ro title='Зарецкий Артем' image='images/Зарецкий_Артем.jpg'/>
+                    <Ro title='Тысячных Алёна' image='images/Тысячных_Алёна.jpg'/>
+                    <Ro title='Братанов Илья' image='images/Братанов_Илья.jpg'/>
+                    <Ro title='Ермашов Владимир' image='images/Ермашов_Владимир.jpg'/>
+                    <Ro title='Россихин Евгений' image='images/Россихин_Евгении.jpg'/>
+                    <Ro title='Тертычный Никита' image='images/Тертычныи_Никита.jpg'/>
+                    <Ro title='Сорокина Вероника' image='images/Сорокина_Вероника.jpg'/>
+                    <Ro title='Чемоданова Катерина' image='images/Чемоданова_Катерина.jpg'/>
+                    <Ro title='Данилин Даниил' image='images/Данилин_Даниил.jpg'/>
+                    <Ro title='Морозова Кира' image='images/Морозова_Кира.jpg'/>
+                    <Ro title='Некрасова Ирина' image='images/Некрасова_Ирина.jpg'/>
+                    <Ro title='Коломина Арина' image='images/Коломина_Арина.jpg'/>
                 </div>
             </div>
         </div>
--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,11 +1,3 @@
-// import { useEffect } from 'react'
-// import { useForm } from 'react-hook-form'
-// import { useNavigate } from 'react-router-dom'
-
-// interface LoginValues {
-//   username: string
-//   password: string
-// }
 
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -92,85 +84,6 @@ const AuthPage = () => {
       </div>
     </div>
   )
-  // const { 
-  //   register, 
-  //   handleSubmit, 
-  //   formState: { errors, isValid } 
-  // } = useForm<LoginValues>({
-  //   mode: 'onChange' // Валидация при изменении полей
-  // })
-
-  // const onSubmit = (data: LoginValues) => {
-  //   console.log('login')
-  //   navigate('/profile') // Переход после успешной валидации
-  // }
-
-  // const navigate = useNavigate()
-
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, []);
-
-  // return (
-  //   <div className="min-h-screen flex items-center justify-center p-2">
-  //     <div className="card w-full max-w-md bg-brand rounded-2xl shadow-lg p-6">
-  //       <h1 className="text-2xl font-bold text-white text-center mb-6 uppercase">АВТОРИЗАЦИЯ</h1>
-        
-  //       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-  //         <div>
-  //           <label className="block text-s font-semibold text-white mb-2">Логин</label>
-  //           <input 
-  //             className="w-full px-4 py-3 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-  //             {...register('username', { 
-  //               required: 'Логин обязателен'
-  //             })}
-  //           />
-  //           {errors.username && <p className="text-red-300 text-xs mt-1">{errors.username.message}</p>}
-  //         </div>
-
-  //         <div>
-  //           <label className="block text-s font-semibold text-white mb-2">Пароль</label>
-  //           <input 
-  //             type="password"
-  //             className="w-full px-4 py-3 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-  //             {...register('password', { 
-  //               required: 'Пароль обязателен'
-  //             })}
-  //           />
-  //           {errors.password && <p className="text-red-300 text-xs mt-1">{errors.password.message}</p>}
-  //         </div>
-
-  //         <div className="text-center mt-2">
-  //           <a href="/reset" className="text-s text-white italic hover:underline">
-  //             Забыли логин или пароль?
-  //           </a>
-  //         </div>
-          
-  //         <button 
-  //           type="submit"
-  //           className={`w-full font-bold rounded-full transition-colors pt-10 ${
-  //             isValid ? '': 'cursor-not-allowed'
-  //           }`}
-  //           disabled={!isValid}
-  //         >
-  //           <h2 className="text-3xl uppercase text-white">ВОЙТИ</h2>
-  //         </button>
-
-  //         <div className="relative my-2">
-  //           <div className="absolute inset-0 flex items-center justify-center">
-  //             <div className="min-w-60 border-t border-gray-300"></div>
-  //           </div>
-  //         </div>
-
-  //         <div className="text-center">
-  //           <h2><a href="/reg" className="inline-block text-s text-white py-4">
-  //             Регистрация
-  //           </a></h2>
-  //         </div>
-  //       </form>
-  //     </div>
-  //   </div>
-  // )
 }
 
 export default AuthPage
