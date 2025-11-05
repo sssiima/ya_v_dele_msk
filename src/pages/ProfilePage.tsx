33f17cb425005258a796888a2d7d0a5f09cc420b
sssiima <viktoria.simanchuk@gmail.com>
2025-11-05 18:15:34 +0300

    video lessons to mk str

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -81,15 +81,23 @@ const ProfilePage = () => {
     const [activeDotpr, setActiveDotpr] = useState(0);
     const scrollContainerRefpod = useRef<HTMLDivElement>(null);
     const [activeDotpod, setActiveDotpod] = useState(0);
+    const scrollContainerRefvid = useRef<HTMLDivElement>(null);
+    const [activeDotvid, setActiveDotvid] = useState(0);
 
   
     const mk_list = [
       { title: "Первый мастер-класс", subtitle: 'Проблема. Идея. Решение', image: '/images/mkfirst.png', link: 'https://drive.google.com/drive/mobile/folders/1sb1L1MynvdLxU6jMOxTQR24-Nudx5-DD/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1I2AEC4OltOGGJWFncj8yS1Nqio9PczF3?sort=13&direction=a', disabled: false },
       { title: "Второй мастер-класс", subtitle: 'Customer development. ЦА.', image: '/images/mksecond.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1PjByX9ckPyi-1ANFeCrDKXq1_cso_4SQ?sort=13&direction=a', disabled: true },
       { title: "Третий мастер-класс", subtitle: 'MVP. HADI - циклы.', image: '/images/mkthird.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
-      { title: "Четвертый мастер-класс", subtitle: 'Бизнес - модель.', image: '/images/mkfourth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
-      { title: "Пятый мастер-класс", subtitle: 'Финансы.', image: '/images/mkfifth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
-      { title: "Шестой мастер-класс", subtitle: 'Маркетинг.', image: '/images/mksixth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
+      { title: "Четвертый мастер-класс", subtitle: 'Бизнес - модель. Базовый трек', image: '/images/mkfourth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
+      { title: "Пятый мастер-класс", subtitle: 'Финансы. Базовый трек', image: '/images/mkfifth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
+      { title: "Шестой мастер-класс", subtitle: 'Маркетинг. Базовый трек', image: '/images/mksixth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
+      { title: "Четвертый мастер-класс", subtitle: 'Бизнес - модель. Социальный трек', image: '/images/mkfourth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
+      { title: "Пятый мастер-класс", subtitle: 'Финансы. Социальный трек', image: '/images/mkfifth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
+      { title: "Шестой мастер-класс", subtitle: 'Маркетинг. Социальный трек', image: '/images/mksixth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
+      { title: "Четвертый мастер-класс", subtitle: 'Бизнес - модель. Инновационный трек', image: '/images/mkfourth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
+      { title: "Пятый мастер-класс", subtitle: 'Финансы. Инновационный трек', image: '/images/mkfifth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
+      { title: "Шестой мастер-класс", subtitle: 'Маркетинг. Инновационный трек', image: '/images/mksixth.png', link: 'https://drive.google.com/drive/mobile/folders/1LOowiL6-yEtmQQSRNx-yhr07QqEnjPcF/1liKGGfATdDN_AvyhNXPN1ivVtvSVSreN?sort=13&direction=a', disabled: true },
   ]
 
   const project_list = [
@@ -109,6 +117,11 @@ const ProfilePage = () => {
       { title: "Про креативность", image: '/images/podcast_create.png', link: 'https://vkvideo.ru/video-210144042_456239575?pl=-210144042_2' },
       { title: "Про страхи и рост", image: '/images/podcast_fear.png', link: 'https://vkvideo.ru/video-210144042_456239501?pl=-210144042_2' },
   ]
+
+  const video_lessons = [
+    { title: "урок 1", subtitle: 'что-то там', image: '/images/' },
+    { title: "урок 2", subtitle: 'что-то там', image: '/images/' },
+]
 
   const handleScrollmk = useCallback(() => {
     const container = scrollContainerRefmk.current;
@@ -171,6 +184,37 @@ const ProfilePage = () => {
         behavior: 'smooth'
       });
     };
+
+    const handleScrollvid = useCallback(() => {
+      const container = scrollContainerRefvid.current;
+      if (!container) return;
+  
+      const scrollLeft = container.scrollLeft;
+      const containerWidth = container.clientWidth;
+      const cardWidth = 205; // w-[120px]
+      const gap = 12; // space-x-3 = 12px
+      
+      // Вычисляем индекс активной карточки
+      const scrollPosition = scrollLeft + containerWidth / 2;
+      const activeIndex = Math.floor(scrollPosition / (cardWidth + gap));
+      
+      setActiveDotvid(Math.min(activeIndex, podcast_list.length - 1));
+    }, [podcast_list.length]);
+  
+    // Функция для скролла к определенной точке
+    const scrollToDotvid = (index: number) => {
+      const container = scrollContainerRefvid.current;
+      if (!container) return;
+  
+      const cardWidth = 205; // w-[200px]
+      const gap = 12; // space-x-3 = 12px
+      const scrollPosition = index * (cardWidth + gap);
+      
+      container.scrollTo({
+        left: scrollPosition,
+        behavior: 'smooth'
+      })
+    }
 
     const handleScrollpod = useCallback(() => {
       const container = scrollContainerRefpod.current;
@@ -1430,6 +1474,39 @@ const ProfilePage = () => {
                   onClick={() => scrollToDotpr(index)}
                   className={`w-2 h-2 rounded-full transition-all duration-300 ${
                     index === activeDotpr ? 'bg-brand scale-125' : 'bg-gray-300'
+                  }`}
+                />
+              ))}
+            </div>
+          </div>
+
+          <h3 className="text-left normal-case text-brand font-extrabold text-[18px] uppercase pb-3">Видео-уроки</h3>
+          <div className="relative">
+            <div 
+              ref={scrollContainerRefvid}
+              className="flex overflow-x-auto space-x-3 hide-scrollbar-none [scrollbar-width:none] [-ms-overflow-style:none] 
+                          [-webkit-overflow-scrolling:touch] snap-x snap-mandatory"
+              onScroll={handleScrollvid}
+            >
+              {video_lessons.map((video, index) => (
+                <div key={index} className="snap-start">
+                  <Card 
+                    title={video.title}
+                    subtitle={video.subtitle}
+                    image={video.image}
+                  />
+                </div>
+              ))}
+            </div>
+            
+            {/* Точки прогресса */}
+            <div className="flex justify-center space-x-2 mt-2">
+              {video_lessons.map((_, index) => (
+                <button 
+                  key={index}
+                  onClick={() => scrollToDotvid(index)}
+                  className={`w-2 h-2 rounded-full transition-all duration-300 ${
+                    index === activeDotvid ? 'bg-brand scale-125' : 'bg-gray-300'
                   }`}
                 />
               ))}
